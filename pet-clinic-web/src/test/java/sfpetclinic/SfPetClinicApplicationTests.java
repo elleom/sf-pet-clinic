cf63eae146b7c17eaa55f6cc7a7300283a9e6856
el_le <el_leo_m@hotmail.com>
2020-11-04 01:11:45 +0100

    refactor

--- pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,6 +1,6 @@
-package com.elleo.sfpetclinic;
+package sfpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
