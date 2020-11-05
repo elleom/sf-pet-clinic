7e3c207f4aa9a2cb2c8ae3d9703bae985a923a21
el_le <el_leo_m@hotmail.com>
2020-11-05 22:32:35 +0100

    ID auto generation . Closes #23.

--- pet-clinic-data/src/main/java/com/elleo/sfpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/elleo/sfpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,13 @@
 package com.elleo.sfpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.elleo.sfpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID,T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long,T> map = new HashMap<>();
+
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
@@ -16,8 +16,18 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+
+        if (object != null) {
+            if (object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else{
+            throw new RuntimeException("Object cannot be null");
+        }
+
+
         return object;
     }
 
@@ -31,6 +41,17 @@ public abstract class AbstractMapService<T, ID> {
 
     T findByLastName(String lastName){
         return null;
+    }
+
+    private Long getNextId(){
+       Long nextId = null;
+       try{
+           nextId =Collections.max(map.keySet()) +1;
+       } catch (NoSuchElementException e){
+           nextId = 1L;
+       }
+       return nextId;
+
     }
 
 }
--- pet-clinic-web/src/main/java/com/elleo/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elleo/sfpetclinic/bootstrap/DataLoader.java
@@ -23,28 +23,28 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setName("Fiona");
         owner2.setLastName("Glennane");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+
         vet2.setName("Jessie");
         vet2.setLastName("Porter");
 
