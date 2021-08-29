cc2cc7774b640f43fc6ab0a1ba4371c663b05dcc
elleom <el_leo_m@hotmail.com>
2021-08-29 12:21:58 +0200

    solved css rundtime generation

--- pet-clinic-web/src/test/java/com/elleo/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/elleo/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,6 +1,6 @@
-package sfpetclinic;
+package com.elleo.sfpetclinic;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
