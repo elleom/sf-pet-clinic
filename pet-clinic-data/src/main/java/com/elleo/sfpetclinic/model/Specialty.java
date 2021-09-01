a2b4da185b1f45cec2bdd7fc46a37ec9867de0a0
elleom <el_leo_m@hotmail.com>
2021-09-01 23:05:10 +0200

    Create specialty class. Closes #27

--- pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Specialty.java
@@ -1,4 +1,13 @@
 package com.elleo.sfpetclinic.model;
 
-public class Specialty {
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
