fd795ef664b3b22d4e74dc11913d21c48267465d
elleom <el_leo_m@hotmail.com>
2021-09-04 21:23:27 +0200

    Added PetType to Bootstrap data. Closes #32

--- pet-clinic-web/src/main/java/com/elleo/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elleo/sfpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.elleo.sfpetclinic.bootstrap;
 
 import com.elleo.sfpetclinic.model.Owner;
+import com.elleo.sfpetclinic.model.PetType;
 import com.elleo.sfpetclinic.model.Vet;
 import com.elleo.sfpetclinic.services.OwnerService;
+import com.elleo.sfpetclinic.services.PetTypeService;
 import com.elleo.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
 
@@ -35,6 +47,8 @@ public class DataLoader implements CommandLineRunner {
         owner2.setLastName("Glennane");
 
         ownerService.save(owner2);
+
+        System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
 
