7dcab68d1e9786c79ef4a7c64280805f07094c60
elleom <el_leo_m@hotmail.com>
2021-10-04 22:19:33 +0200

    Create JPA Mapping Vet.java   Closes #37

--- pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Speciality.java
@@ -1,6 +1,17 @@
 package com.elleo.sfpetclinic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+
+    /***
+     * IMPORTANT:
+     * does not need a vet mapping cause all is mapped on vet itself;
+     */
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
