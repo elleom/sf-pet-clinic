48d0a203d98978beed657effb0a28477ff14c26d
elleom <el_leo_m@hotmail.com>
2021-10-09 14:27:07 +0200

    Added vets jpa service. Closes #41

--- pet-clinic-data/src/main/java/com/elleo/sfpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/com/elleo/sfpetclinic/repositories/VetRepository.java
@@ -1,8 +1,11 @@
 package com.elleo.sfpetclinic.repositories;
 
+import com.elleo.sfpetclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * @author el_le
  * @since 05/10/2021 21:53
  */
-public interface VetRepository {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
