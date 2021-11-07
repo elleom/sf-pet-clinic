4c77031691907e1ed3cab69f1351dac2b9f8dff5
elleom <el_leo_m@hotmail.com>
2021-11-07 14:34:57 +0100

    add tests cases ownerMapService

---
+++ pet-clinic-data/src/test/java/com/elleo/sfpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,68 @@
+package com.elleo.sfpetclinic.services.map;
+
+import com.elleo.sfpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author el_le
+ * @since 04/11/2021 17:44
+ */
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    Long ownerId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(ownerId).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet =ownerMapService.findAll();
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = Owner.builder().id(id).build();
+        Owner savedOwner = ownerMapService.save(owner2);
+        assertEquals(id, savedOwner.getId());
+
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
