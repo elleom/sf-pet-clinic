e5e0e8cf6a1c185b13359eb94be35bde5ec7faf4
elleom <el_leo_m@hotmail.com>
2021-11-16 13:33:01 +0100

    add webBinder

--- pet-clinic-web/src/main/java/com/elleo/sfpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/elleo/sfpetclinic/controller/OwnerController.java
@@ -5,7 +5,9 @@ import com.elleo.sfpetclinic.model.Pet;
 import com.elleo.sfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +21,12 @@ public class OwnerController {
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
 
     @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwners(Model model){
--- pet-clinic-web/src/main/java/com/elleo/sfpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/elleo/sfpetclinic/controller/VetController.java
@@ -3,6 +3,8 @@ package com.elleo.sfpetclinic.controller;
 import com.elleo.sfpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,11 @@ public class VetController {
 
     public VetController(VetService vetService) {
         this.vetService = vetService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"/vets.html", "/vets"})
