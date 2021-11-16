ed4bb7e924650e4f57c4d7d4d6da30271fa0986d
elleom <el_leo_m@hotmail.com>
2021-11-16 16:37:08 +0100

    add Visit functionality. Closes #56

--- pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/elleo/sfpetclinic/model/Pet.java
@@ -24,6 +24,10 @@ public class Pet extends BaseEntity {
         this.owner = owner;
         this.birthDate = birthDate;
         this.visits = visits;
+
+        if (visits == null || visits.size() > 0 ){
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
